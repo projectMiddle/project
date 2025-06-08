99988d18c1ac81ac03b3c8e3dcea7b5fb3db117e
mia-OwO <Ruing.OqO@gmail.com>
2025-06-09 00:39:42 +0900

    attendanceService (login,logout)

--- soltech/project/src/main/java/com/example/project/repository/AttendanceRepository.java
+++ soltech/project/src/main/java/com/example/project/repository/AttendanceRepository.java
@@ -1,9 +1,13 @@
 package com.example.project.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.project.entity.Attendance;
+import com.example.project.entity.Employee;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-    
+    Attendance findByEmpNoAndAttWorkDate(Employee empNo, LocalDate attWorkDate );
 }
--- soltech/project/src/main/java/com/example/project/service/AttendanceService.java
+++ soltech/project/src/main/java/com/example/project/service/AttendanceService.java
@@ -1,7 +1,15 @@
 package com.example.project.service;
 
+import java.beans.Transient;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Service;
 
+import com.example.project.dto.AttendanceDTO;
+import com.example.project.entity.Attendance;
+import com.example.project.entity.Employee;
+import com.example.project.entity.constant.AttStatus;
 import com.example.project.repository.AttendanceRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,5 +21,62 @@ import lombok.extern.log4j.Log4j2;
 public class AttendanceService {
     
     private final AttendanceRepository attendanceRepository;
+
+
+
+    // 출근
+    public AttendanceDTO login(Employee employee){
+         Attendance attendance = Attendance.builder()
+         .empNo(employee)
+         .attWorkDate(LocalDate.now())
+         .attStartTime(LocalTime.now())
+         .attEndTime(null)
+         .attStatus(AttStatus.WORK)
+         .build();
+
+        return entityDto(  attendanceRepository.save(attendance), employee);
+    }
+
+
+    // 퇴근
+    @Transient
+   public AttendanceDTO logout(Employee employee){
+    Attendance attendance = attendanceRepository.findByEmpNoAndAttWorkDate( employee, LocalDate.now());
+    attendance.changeAttEndTime(LocalTime.now());
+    attendance.changeAttStatus(AttStatus.OFF);
+
+    return entityDto(attendance, employee);
+
+
+}
+
+
+    // // 불 반짝
+    // public boolean working(Employee employee){
+    //    return attendanceRepository.findByEmpNoAndAttWorkDate(employee, LocalDate.now())
+    //     .map(att ->att.getAttStatus() == AttStatus.WORK)
+    //     .orElse(false);
+    // }
+
+
+  
+
+
+
+
+
+    //entity => dto
+    private AttendanceDTO entityDto(Attendance attendance, Employee employee){
+        AttendanceDTO dto = AttendanceDTO.builder()
+        .attNo(attendance.getAttNo())
+        .empNo(employee.getEmpNo())
+        .attWorkDate(attendance.getAttWorkDate())
+        .attStartTime(attendance.getAttStartTime())
+        .attEndTime(attendance.getAttEndTime())
+        .attStatus(attendance.getAttStatus())
+        .build();
+
+        return dto;
+    }
 
 }
