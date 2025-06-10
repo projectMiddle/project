fa842907d1decab8578f74e7c82e57cdc76842f1
louis-ys <lye98055@gmail.com>
2025-06-10 17:29:47 +0900

    스케줄 기능 구현

--- soltechreact/src/App.jsx
+++ soltechreact/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/schedule"
+          element={
+            <div className="App">
+              <h1 style={{ textAlign: "left", margin: "20px" }}>캘린더</h1>
+              <CalendarView />
+            </div>
+          }
+        />
+        {/* <Link to="/schedule">스케줄</Link> 메뉴에서 버튼으로 사용할때  */}
+        {/* 메인 페이지용 Route (선택사항) */}
+        <Route path="/" element={<h1 style={{ padding: "40px" }}>팀 공통 메인 페이지</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ soltechreact/src/components/CalendarView.jsx
@@ -0,0 +1,81 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useState } from "react";
+import dayjs from "dayjs";
+
+const CalendarView = () => {
+  const [events, _setEvents] = useState([
+    {
+      id: "1",
+      title: "프로젝트 1차 기안 작성 및 ppt 발표",
+      start: dayjs("2025-06-09T09:00").toISOString(),
+      end: dayjs("2025-06-18T10:00").toISOString(),
+      type: "DEPARTMENT",
+      color: "#D580F6", // 부서 일정 (초록색)
+    },
+    {
+      id: "2",
+      title: "연차",
+      start: dayjs("2025-06-16T13:00").toISOString(),
+      end: dayjs("2025-06-18T14:00").toISOString(),
+      type: "PERSONAL",
+      color: "#2196F3", // 개인 일정 (파랑)
+    },
+    {
+      id: "3",
+      title: "현충일",
+      start: dayjs("2025-06-06T13:00").toISOString(),
+      end: dayjs("2025-06-06T23:00").toISOString(),
+      color: "#ED1115", // 공휴일 (빨강 )
+      classNames: ["holiday-event"],
+    },
+    {
+      id: "3",
+      title: "대통령 선거",
+      start: dayjs("2025-06-02T13:00").toISOString(),
+      end: dayjs("2025-06-02T23:00").toISOString(),
+      color: "#ED1115", // 공휴일 (빨강 )
+      classNames: ["holiday-event"],
+    },
+  ]);
+
+  return (
+    <div className="p-4">
+      <div className="calendar-container">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            start: "prev,next today",
+            center: "title",
+            end: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          buttonText={{
+            today: "오늘",
+            month: "월간",
+            week: "주간",
+            day: "일간",
+          }}
+          events={events}
+          height={"auto"}
+          displayEventTime={false}
+        />
+        <footer className="calendar-legend">
+          <span>
+            <span className="dot holiday"></span>공휴일
+          </span>
+          <span>
+            <span className="dot personal"></span>개인일정
+          </span>
+          <span>
+            <span className="dot department"></span>부서일정
+          </span>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarView;
