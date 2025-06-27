6833c398ae334fcda2337fc52d42ad7ceb51cd97
rhdrks9898 <rhdrks9898@gmail.com>
2025-06-27 12:36:43 +0900

    Delete soltechreact directory

--- soltechreact/src/routes/MailRoute.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MailRoute = () => {
-  return <div></div>;
-};
-
-export default MailRoute;
--- soltechreact/src/test/Dongjin.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dongjin = () => {
-  return <div></div>;
-};
-
-export default Dongjin;
