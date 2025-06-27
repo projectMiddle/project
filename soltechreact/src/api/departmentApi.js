ef47c232236a3ae32ef61b4f1baad3b961312dc0
joung dong jin <jdj5201@gmail.com>
2025-06-27 13:13:43 +0900

    부서 수정

--- soltechreact/src/api/departmentApi.js
+++ soltechreact/src/api/departmentApi.js
@@ -1,14 +1,14 @@
 export const API_SERVER_HOST = "/intrasoltech/department";
+import api from "./axios";
 
 export const getDepartments = async () => {
-  const res = await fetch(`${API_SERVER_HOST}/list`);
-  if (!res.ok) throw new Error("부서 조회 실패");
-  return res.json();
+  const res = await api.get(`${API_SERVER_HOST}/list`);
+  console.log("부서 res : ", res);
+  return res.data;
 };
 
 export const getEmployeesByDepartment = async (deptNo) => {
   console.log("부서별 사원 조회 요청:", deptNo);
-  const res = await fetch(`${API_SERVER_HOST}/list/${deptNo}`);
-  if (!res.ok) throw new Error("부서별 사원 조회 실패");
-  return res.json();
+  const res = await api.get(`${API_SERVER_HOST}/list/${deptNo}`);
+  return res.data;
 };
